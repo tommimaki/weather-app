71fe11f16b85455054b0b428a8050408df54af1a
Tommi <tommi@Tommis-MacBook-Pro.local>
2022-11-10 13:30:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+
+  const [data, setData] = useState({});
+  const [kaupunki, setKaupunki] = useState('');
+  const [cels, setCels] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${kaupunki}&appid=6671217914dc1325bbc0ef6addde7055`;
+
+
+
+  const fetchKaupunki = () => {
+   
+      axios.get(url)
+        .then(res => {
+          setData(res.data);
+          console.log(res.data);
+          setKaupunki('');
+        })
+    }
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Weather App</h1>
+      
+      <input
+       value={kaupunki}
+      onChange={e => setKaupunki(e.target.value) } />
+      <button type='submit' onClick={fetchKaupunki}>Hae</button>
+
+      <div>
+        <h2>Weather in {data.weather.main}</h2>
+        {data.weather && <p> Weather: {data.weather[0].main}</p>}
+        {data.main && <p>Temperature: {data.main.temp}</p>}
+        {data.main && <p>Humidity: {data.main.humidity}</p> }
+    
+
+      </div>
+      </div>
+
+      
   );
 }
 
